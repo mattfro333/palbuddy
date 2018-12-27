72990afd9613cb6453f021f0000e8e5c32fbe1e5
mattfro333 <mattfro333@gmail.com>
2018-12-26 23:38:30 -0700

    get and post routes are wire framed

--- server.js
+++ server.js
@@ -7,11 +7,44 @@
 
     fs.readFile("./templates/home.html", (err, results)=>{
       res.send(results.toString());
-    })
+    });
+  });
+
+  server.get('/success/:orderID', (req, res)=>{
 
   });
 
-  
+  server.get('/cancel/:orderID', (req, res)=>{
+
+  });
+
+  server.get('/orderdetails/:orderID', (req, res)=>{
+
+  });
+
+  server.get('/refund/:orderID', (req, res)=>{
+
+  });
+
+  server.get('/recurring_succes/:planID', (req, res)=>{
+
+  });
+
+  server.get('/recurring_cancel/:planID', (req, res)=>{
+
+  });
+
+  server.get('/recurring_orderdetails/:agreementID', (req, res)=>{
+
+  });
+
+  server.post('/buysingle', (req, res)=>{
+
+  });
+
+  server.post('/buyrecurring', (req, res)=>{
+
+  });
 
   server.listen(8080, "localhost", (err)=>{
     console.log(err || "server Online");
