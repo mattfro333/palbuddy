8969306a6774a0425431fa88fca41a48c5261ac0
mattfro333 <mattfro333@gmail.com>
2018-12-26 23:32:05 -0700

    home route complete

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-(()=>)
+((express, server, bodyParser, fs)=>{
+
+  server.use(bodyParser.urlencoded({ extended: true }));
+  server.use(express.static("pub"));
+
+  server.get('/', (req, res)=>{
+
+    fs.readFile("./templates/home.html", (err, results)=>{
+      res.send(results.toString());
+    })
+
+  });
+
+  
+
+  server.listen(8080, "localhost", (err)=>{
+    console.log(err || "server Online");
+  });
+
+})
 (
   require('express'),
   require('express')(),
   require('body-parser'),
   require('fs')
-)
+);
