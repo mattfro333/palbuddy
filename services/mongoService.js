0fb7c6a56de68aa9b388e9572cc3496328eaefde
mattfro333 <mattfro333@gmail.com>
2018-12-28 11:49:01 -0700

    working on a config file

--- services/mongoService.js
+++ services/mongoService.js
@@ -4,19 +4,14 @@
     "mongodb://localhost:27017/palbuddy";
 
   var Connect = (cb)=>{
-
     mongodb.connect(connectionString, (err, db)=>{
-
       return cb(err, db, ()=>{ db.close(); });
     });
   };
 
   mongoService.Create = (colName, createObj, cb)=>{
-
     Connect((err, db, close)=>{
-
       db.collection(colName).insert(createObj, (err, results)=>{
-
         cb(err, results);
         return close();
       });
@@ -24,9 +19,7 @@
   };
 
   mongoService.Read = (colName, readObj, cb)=>{
-
     Connect((err, db, close)=>{
-
       db.collection(colName).find(readObj).toArray((err, results)=>{
         cb(err, results);
         return close();
@@ -37,7 +30,6 @@
   mongoService.Update = (colName, findObj, updateObj, cb)=>{
     Connect((err, db, close)=>{
       db.collection(colName).update(findObj, { $set: updateObj}, (err, results)=>{
-
         cb(err, results);
         return close();
       });
