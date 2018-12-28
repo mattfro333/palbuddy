707acb45b639556372c6278eae944db451e86e98
mattfro333 <mattfro333@gmail.com>
2018-12-28 11:39:26 -0700

    creating a mongo service

---
+++ services/mongoService.js
@@ -0,0 +1,51 @@
+((mongoService, mongodb)=>{
+
+  var connectionString = process.envMongoConnections ||
+    "mongodb://localhost:27017/palbuddy";
+
+  var Connect = (cb)=>{
+
+    mongodb.connect(connectionString, (err, db)=>{
+
+      return cb(err, db, ()=>{ db.close(); });
+    });
+  };
+
+  mongoService.Create = (colName, createObj, cb)=>{
+
+    Connect((err, db, close)=>{
+
+      db.collection(colName).insert(createObj, (err, results)=>{
+
+        cb(err, results);
+        return close();
+      });
+    });
+  };
+
+  mongoService.Read = (colName, readObj, cb)=>{
+
+    Connect((err, db, close)=>{
+
+      db.collection(colName).find(readObj).toArray((err, results)=>{
+        cb(err, results);
+        return close();
+      });
+    });
+  };
+
+  mongoService.Update = (colName, findObj, updateObj, cb)=>{
+    Connect((err, db, close)=>{
+      db.collection(colName).update(findObj, { $set: updateObj}, (err, results)=>{
+
+        cb(err, results);
+        return close();
+      })
+    });
+  }
+
+})
+(
+  module.exports,
+  require('mongodb')
+);
