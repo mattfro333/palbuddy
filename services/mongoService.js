ebe3999815f4f97e4a7dbe329222515b8a894ebd
mattfro333 <mattfro333@gmail.com>
2019-01-02 22:28:48 -0700

    twirling and twirling to victory

--- services/mongoService.js
+++ services/mongoService.js
@@ -1,52 +1,65 @@
-((mongoService, mongodb)=>{
+((mongoService, mongodb) => {
 
-  var connectionString = process.envMongoConnections ||
-    "mongodb://localhost:27017/palbuddy";
+    const url = "mongodb://localhost:27017";
+    const dbName = "paypaltesting";
 
-  var Connect = (cb)=>{
-    mongodb.connect(connectionString, (err, db)=>{
-      return cb(err, db, ()=>{ db.close(); });
-    });
-  };
+    var Connect = (cb) => {
+        mongodb.connect(url, { useNewUrlParser: true }, (err, client) => {
+            return cb(err, client.db(dbName), () => {
+                client.close();
+            });
+        });
+    };
 
-  mongoService.Create = (colName, createObj, cb)=>{
-    Connect((err, db, close)=>{
-      db.collection(colName).insert(createObj, (err, results)=>{
-        cb(err, results);
-        return close();
-      });
-    });
-  };
+    mongoService.Create = (colName, createObj, cb) => {
+        Connect((err, db, close) => {
+            db.collection(colName)
+                .insertOne(createObj)
+                .then(result => {
+                    // console.log("result, mongoService.Create: " + JSON.stringify(result));
+                    // const { insertedId } = result;
+                    // console.log(`Inserted document with _id: ${insertedId}`);
+                    // console.log("InsertedId: " + result.insertedId);
+                    cb(result);
+                    return close();
+                })
+                .catch(err => console.error(err.message));
+        });
+    };
 
-  mongoService.Read = (colName, readObj, cb)=>{
-    Connect((err, db, close)=>{
-      db.collection(colName).find(readObj).toArray((err, results)=>{
-        cb(err, results);
-        return close();
-      });
-    });
-  };
+    mongoService.Read = (colName, readObj, cb) => {
+        Connect((err, db, close) => {
+            db.collection(colName).find(readObj).toArray((err, results) => {
+                cb(err, results);
+                return close();
+            });
+        });
+    };
 
-  mongoService.Update = (colName, findObj, updateObj, cb)=>{
-    Connect((err, db, close)=>{
-      db.collection(colName).update(findObj, { $set: updateObj}, (err, results)=>{
-        cb(err, results);
-        return close();
-      });
-    });
-  }
+    mongoService.UpdateOne = (colName, findObj, updateObj, cb) => {
+        Connect((err, db, close) => {
+            console.log("findObj, mongoService.UpdateOne: " + JSON.stringify(findObj));
+            // console.log("updateObj, mongoService.Update: " + JSON.stringify(updateObj));
+            db.collection(colName).updateOne(findObj, { $set: updateObj })
+                .then(results => {
+                    console.log("results, mongoService.UpdateOne: " + JSON.stringify(results));
+                    cb(results);
+                    return close();
+                })
+                .catch(err => { console.error(err.meesage) });
+        });
+    };
 
-  mongoService.Delete = (colName, findObj, cb)=>{
-    Connect((err, db, close)=>{
-    db.collection(colName).remove(findObj, (err, results)=>{
-      cb(err, results);
-      return close();
-    });
-  });
-  }
-
+    mongoService.Delete = (colName, findObj, cb) => {
+        Connect((err, db, close) => {
+            db.collection(colName).remove(findObj, (err) => {
+                cb(err);
+                return close();
+            });
+        });
+    };
 })
-(
-  module.exports,
-  require('mongodb')
-);
+    (
+    module.exports,
+    require('mongodb')
+    )
